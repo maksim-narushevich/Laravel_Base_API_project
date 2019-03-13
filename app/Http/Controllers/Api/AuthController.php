9de80e043c36518e86dd482e258d3c461081551e
maxim.narushevich <maxi@digitalmind.be>
2019-03-13 12:01:11 +0300

    Initial commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers\Api;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends Controller
+{
+    public $successStatus = 200;
+
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 401);                        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('AppName')->accessToken;
+        return response()->json(['success'=>$success], $this->successStatus);
+    }
+
+
+    public function login(){
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('AppName')-> accessToken;
+            return response()->json(['success' => $success], $this-> successStatus);
+        } else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
+    }
+
+    public function getUser() {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('v1')->group(function(){
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('getUser', 'Api\AuthController@getUser');
+    });
+});
