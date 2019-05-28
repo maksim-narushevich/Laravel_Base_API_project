10de5dc9652133bf340f24f8d39602f639f9455d
Maksim <narushevich.maksim@gmail.com>
2019-05-28 18:04:13 +0300

    Update init.sql

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,1 +1,4 @@
+INSERT INTO mysql.user (Host, User, Password) VALUES ('%', 'root', password('laraapi'));
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
 CREATE DATABASE IF NOT EXISTS laravel_api_db;
