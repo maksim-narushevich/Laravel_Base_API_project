eb31048deb4a4965e664283d2f8ca07fa90aa17d
Maksim Narushevich <maxim.narushevich@cactussoft.biz>
2019-06-07 16:06:45 +0300

    - Prevent functionality to send emails while test mode

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -82,15 +82,16 @@ class AuthController extends BaseApiController
 
         $user = User::create($input);
 
-//        //-- Send email after successful registration
-//        if(!is_null($user) && !empty($user->email)){
-//            $emailData['content']="Welcome to this app ".$user->name."!";
-//            $emailData['user']=$user;
-//            Mailer::sendSuccessRegistrationMail($emailData);
-//        }
+        //-- Get current environment in order prevent sending email in 'test' mode
+        $env = app()->environment();
+        //-- Send email after successful registration
+        if($env!=='test' && !is_null($user) && !empty($user->email)){
+            $emailData['content']="Welcome to this app ".$user->name."!";
+            $emailData['user']=$user;
+            Mailer::sendSuccessRegistrationMail($emailData);
+        }
 
-
-        $success['token'] =  "ddd";
+        $success['token'] =  $user->createToken('AppName')->accessToken;
         return response()->json(['success'=>$success], $this->successStatus);
     }
 
