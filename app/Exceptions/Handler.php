0494769f5e8245631b7b3794a448d680b87adcc2
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-07-18 00:06:36 +0300

    - Updated environment files

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,6 +42,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if (app()->bound('sentry') && $this->shouldReport($exception)){
+            app('sentry')->captureException($exception);
+        }
         parent::report($exception);
     }
 
